c90d69ad0e5dbdd02437a66ab2d0dbc3e6309bea
rsofFed1 <fed1@rsof.com.sa>
2025-06-13 15:54:48 +0500

    fix: header menu right side extra space fix

--- Frontend/src/layouts/header-parts/header-menu-right.tsx
+++ Frontend/src/layouts/header-parts/header-menu-right.tsx
@@ -7,7 +7,7 @@ import { useUserStore } from "@/store/user.store";
 export default function HeaderMenuRight() {
   const { userInfo } = useUserStore();
   return (
-    <div className="ms-auto grid shrink-0 grid-cols-4 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
+    <div className="ms-auto grid shrink-0 grid-cols-2 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
       <ThemeSwitcher />
       <ProfileMenu />
       {
