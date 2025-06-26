d7b76026809164b2260489fcf80262455aa74f13
Hamza Bashir <hamza.bashir@nestosh.com>
2025-06-26 14:41:19 +0500

    implement status update endpoint with transition validation

---
+++ Backend/Sadef.Application/DTOs/LeadDtos/UpdateLeadStatusDto.cs
@@ -0,0 +1,15 @@
+﻿using Sadef.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sadef.Application.DTOs.LeadDtos
+{
+    public class UpdateLeadStatusDto
+    {
+        public required int id { get; set; }
+        public LeadStatus? status { get; set; }
+    }
+}
