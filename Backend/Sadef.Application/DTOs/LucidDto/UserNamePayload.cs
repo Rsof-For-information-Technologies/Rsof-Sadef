d5b41279544231d09013c03e16f5698ba26d7b5c
bed-ahmed <bed3@rsof.com.sa>
2025-07-15 14:48:14 +0500

    user name updated field

---
+++ Backend/Sadef.Application/DTOs/LucidDto/UserNamePayload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sadef.Application.DTOs.LucidDto
+{
+    public class UserNamePayload
+    {
+        public string Name { get; set; }
+
+    }
+}
