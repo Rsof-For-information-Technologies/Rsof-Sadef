505e88c61f28bedb6f9539d299384b38aab8c6e3
bed-ahmed <bed3@rsof.com.sa>
2025-09-02 15:38:43 +0500

    update notification

--- Backend/Sadef.Application/Abstractions/Interfaces/IFirebaseNotificationService.cs
+++ Backend/Sadef.Application/Abstractions/Interfaces/IFirebaseNotificationService.cs
@@ -5,12 +5,10 @@ namespace Sadef.Application.Abstractions.Interfaces
 {
     public interface IFirebaseNotificationService
     {
-        Task<bool> SendNotificationAsync(string title, string body, string deviceToken, IDictionary<string, string>? data = null);
-        Task<bool> SendNotificationToMultipleAsync(string title, string body, List<string> deviceTokens, IDictionary<string, string>? data = null);
         Task<bool> SendNotificationToTopicAsync(string title, string body, string topic, IDictionary<string, string>? data = null);
         Task<Response<DeviceTokenDto>> RegisterDeviceToken(string UserID, string FcmToken, string DeviceType);
         Task<Response<string>> UnregisterDeviceToken(UnregisterDeviceTokenDto dto);
-        Task SendLeadCreatedNotificationToAdminsAsync(string title, string body, IDictionary<string, string>? data = null);
+        Task<bool> SendLeadCreatedNotificationToAdminsAsync(string title, string body, IDictionary<string, string>? data = null);
         Task<bool> TestSendNotificationToMultipleAsync(string title, string body, string userId, IDictionary<string, string>? data = null);
     }
 }
