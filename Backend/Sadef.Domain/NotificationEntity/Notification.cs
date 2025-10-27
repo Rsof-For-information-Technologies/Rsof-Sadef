78cc29f985b8f9ad7267907a4e4d6619d6f2eced
bed-ahmed <bed3@rsof.com.sa>
2025-10-27 12:58:25 +0500

    updated naming to lower case to fix issue during workflow action

---
+++ Backend/Sadef.Domain/NotificationEntity/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sadef.Domain.NotificationEntity
+{
+    public class Notification
+    {
+        public required Guid UserId { get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public bool? isReaded { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    }
+}
